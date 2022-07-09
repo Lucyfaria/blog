ee18fcee828649479fb91fea0fefab700d68c6d0
Lucyfaria <nina.westphal@gmx.net>
2022-07-09 17:07:51 +0200

    form, edit, index and show bootstraped

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :fractions
-  get 'home/frak'
+  # get 'fractions'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
