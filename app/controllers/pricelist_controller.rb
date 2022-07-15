36fd4d37535137761a8dc46c562747a667f98838
Lucyfaria <nina.westphal@gmx.net>
2022-07-15 13:19:53 +0200

    jonas weiß alles

--- app/controllers/pricelist_controller.rb
+++ app/controllers/pricelist_controller.rb
@@ -1,4 +1,12 @@
 class PricelistController < ApplicationController
   def index
+    @fractions = Hash[Fraction.all.map do |fraction|
+      [
+        fraction.name,
+        {
+          primary: fraction.primary
+        }
+      ]
+    end]
   end
 end
