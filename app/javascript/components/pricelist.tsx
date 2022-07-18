408e4267f13ad8b514de78c41155985e861b4142
Lucyfaria <nina.westphal@gmx.net>
2022-07-18 16:37:05 +0200

    try adding react and ts

---
+++ app/javascript/components/pricelist.tsx
@@ -0,0 +1,12 @@
+// app/javascript/entrypoint.tsx
+// import * as React from 'react'
+// import * as ReactDOM from 'react-dom'
+
+// const App = () => {
+//   return (<div>Hello, Rails 7!</div>)
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const rootEl = document.getElementById('app')
+//   ReactDOM.render(<AppWithState />, rootEl)
+// })
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,8 @@
 
 pin "application", preload: true
 pin "select", preload: true
+pin "react", to: "https://ga.jspm.io/npm:react@18.2.0/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.2.0/index.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.24/nodelibs/browser/process-production.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.23.0/index.js"
+pin_all_from "app/javascript/components", under: "components"
